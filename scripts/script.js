9e9863e02fc2e3752b7b5bd822c2b564d2149f71
Haider <halsudani@my.bcit.ca>
2024-10-15 21:41:57 -0700

    added html, js, css codes for demo7

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,19 @@
 
 
 function sayHello() {
-    //do something
+  //do something
 }
 //sayHello();    //invoke function
+
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+    console.log("logging out user");
+  }).catch((error) => {
+    // An error happened.
+  });
+}
